e0e9ba402656fbdabb6b8ddb55845b0207c8257e
Nilesh2380 <nileshkkhune22@navgurukul.org>
2024-04-04 18:35:04 +0530

    create color box

---
+++ script.js
@@ -0,0 +1,25 @@
+let colorselect=document.getElementById('main-container');
+function red(){
+    colorselect.style.backgroundColor = 'red'
+}
+function yellow(){
+    colorselect.style.backgroundColor = 'yellow'
+}
+function blue(){
+    colorselect.style.backgroundColor = 'blue'
+}
+function green(){
+    colorselect.style.backgroundColor = 'green'
+}
+function purple(){
+    colorselect.style.backgroundColor = 'purple'
+}
+function orange(){
+    colorselect.style.backgroundColor = 'orange'
+}
+function pink(){
+    colorselect.style.backgroundColor = 'pink'
+}
+function cyan(){
+    colorselect.style.backgroundColor = 'cyan'
+}
